88768d961c2e19bcb4d2b62b0d927ca51e24fa07
happy_bz <happy_bz@qq.com>
2020-05-31 17:23:28 +0800

    4.微服务架构编码构建 end

--- cloud-provider-payment8001/src/main/java/com/happybz/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/happybz/springcloud/controller/PaymentController.java
@@ -6,10 +6,7 @@ import com.happybz.springcloud.service.PaymentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -23,7 +20,7 @@ public class PaymentController {
 
     @PostMapping(value = "payment/create")
     @ApiOperation(notes = "create", value = "create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("*****insert:" + result);
         if(result>0){
