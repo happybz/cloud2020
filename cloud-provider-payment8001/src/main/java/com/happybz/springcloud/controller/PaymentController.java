744eead24421a49e853b100c5fb2c35ea69db524
happy_bz <happy_bz@qq.com>
2020-05-31 16:23:08 +0800

    业务类

---
+++ cloud-provider-payment8001/src/main/java/com/happybz/springcloud/controller/PaymentController.java
@@ -0,0 +1,47 @@
+package com.happybz.springcloud.controller;
+
+import com.happybz.springcloud.entities.CommonResult;
+import com.happybz.springcloud.entities.Payment;
+import com.happybz.springcloud.service.PaymentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+@Api(value = "PaymentController", description = "HelloController")
+public class PaymentController {
+
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping(value = "payment/create")
+    @ApiOperation(notes = "create", value = "create")
+    public CommonResult create(Payment payment){
+        int result = paymentService.create(payment);
+        log.info("*****insert:" + result);
+        if(result>0){
+            return new CommonResult(200,"success",result);
+        }else{
+            return new CommonResult(444,"failed",null);
+        }
+    }
+
+    @GetMapping(value = "payment/get/{id}")
+    @ApiOperation(notes = "getPaymentById", value = "getPaymentById")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*****get:" + payment);
+        if(payment != null){
+            return new CommonResult(200,"success",payment);
+        }else{
+            return new CommonResult(444,"failed",null);
+        }
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/happybz/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.happybz.springcloud.service.impl;
+
+import com.happybz.springcloud.dao.PaymentDao;
+import com.happybz.springcloud.entities.Payment;
+import com.happybz.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
